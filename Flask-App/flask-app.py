87b19948b701975b208e6d4db21046a1823e19cd
ptrelease <ptrelease@gmail.com>
2015-01-12 14:29:51 +0000

    first commit

---
+++ Flask-App/flask-app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello():
+  a = 'hello'
+  b = 'world'
+  return a+b
+  
+   
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
