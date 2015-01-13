5e8184d3bead7f27d65c29af3290bbcc0dae4e50
treacle74 <ptrelease@gmail.com>
2015-01-13 12:25:15 +0000

    test

--- Flask-App/flask-app.py
+++ Flask-App/flask-app.py
@@ -6,14 +6,14 @@ def hello():
   a = 'hello'
   b = 'world'
   return a+b
-  
 
-@app.route('/goodbye')
+
+@app.route('/goodbye and stuff')
 def goodbye():
   a = 'goodbye'
   b = 'world'
   return a+b
-  
-   
+
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
